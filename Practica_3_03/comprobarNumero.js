6efd64c10e6194a21bb4f0e906c893606a587050
HerrecuDev <phercue2805>
2025-10-27 21:51:50 +0100

    ok

---
+++ Practica_3_03/comprobarNumero.js
@@ -0,0 +1,8 @@
+let numero = prompt("Escribe un numero");
+if (isNaN(Number(numero))) {
+    alert("El caracter introducido NO ES UN NUMERO")
+    
+} else {
+    alert("Si es un número")
+    
+}
---
+++ Practica_3_04/modificarColor.js
@@ -0,0 +1,9 @@
+let rojo = parseInt(Math.random()*255);
+let verde = parseInt(Math.random()*255);
+let azul = parseInt(Math.random()*255);
+
+
+document.write(
+
+
+)
